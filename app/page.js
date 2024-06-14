c8f9c811c0a38da364fe366f57d54f307c679c00
Alejandro Delgado <ialexx9919@gmail.com>
2024-06-14 20:42:35 +0200

    adding-reset-button-popup

--- app/page.js
+++ app/page.js
@@ -14,7 +14,7 @@ const winCombs = [
 ];
 
 export default function Home() {
-  const [userTurn, setUserTurn] = useState(true); // state of the user turn
+  const [userTurn, setUserTurn] = useState(true);
   const [boardState, setBoardState] = useState({
     0: "",
     1: "",
@@ -25,9 +25,10 @@ export default function Home() {
     6: "",
     7: "",
     8: "",
-  }); // state of each cell
-  const [winner, setWinner] = useState(false); // state of the winner
-  const [draw, setDraw] = useState(false); // state of draw
+  });
+  const [winner, setWinner] = useState(false);
+  const [title, setTitle] = useState("");
+  const [winnerCombo, setWinnerCombo] = useState([]);
 
   useEffect(() => {
     checkResult(boardState);
@@ -35,7 +36,6 @@ export default function Home() {
 
   const updateBoard = (idx) => {
     if (!boardState[idx] && !winner) {
-      //will check whether specify idx is empty or not
       let value = userTurn === true ? "X" : "O";
       setBoardState({ ...boardState, [idx]: value });
       setUserTurn(!userTurn);
@@ -49,9 +49,32 @@ export default function Home() {
       const [a, b, c] = comb;
       if (board[a] && board[a] === board[b] && board[a] === board[c]) {
         setWinner(true);
+        setWinnerCombo([a, b, c]);
+        setTitle(`${!userTurn ? "Victoria!" : "Derrota!"}`);
+        return;
       }
-      if (allFilled && !winner) setDraw(true);
+      if (allFilled && !winner) {
+        setTitle("Empate!");
+      }
     }
+  };
+
+  const reset = () => {
+    setBoardState({
+      0: "",
+      1: "",
+      2: "",
+      3: "",
+      4: "",
+      5: "",
+      6: "",
+      7: "",
+      8: "",
+    });
+    setUserTurn(true);
+    setWinner(false);
+    setWinnerCombo([]);
+    setTitle("");
   };
 
   return (
@@ -62,14 +85,19 @@ export default function Home() {
       <div className="flex flex-col items-center">
         <div className="text-center text-2xl mb-2">
           <p>{userTurn === true ? "Es tu turno!" : "Turno de la IA"}</p>
-          <p>{`Game Draw: ${draw}`}</p>
         </div>
         <div className="grid grid-cols-[repeat(3,1fr)] gap-2">
           {[...Array(9)].map((v, idx) => {
             return (
               <div
                 key={idx}
-                className="bg-[#eee] shadow-[0px_4px_#ddd] text-center text-[64px] leading-[100px] font-[bold] w-[100px] h-[100px] cursor-pointer rounded-lg"
+                className={`bg-[#eee] ${
+                  userTurn ? "shadow-[0px_4px_#ddd]" : "shadow-none"
+                } text-center text-[64px] leading-[100px] font-[bold] w-[100px] h-[100px] cursor-pointer rounded-lg ${
+                  winnerCombo.includes(idx)
+                    ? "bg-black text-white shadow-none"
+                    : ""
+                }`}
                 onClick={() => {
                   updateBoard(idx);
                 }}
@@ -79,6 +107,20 @@ export default function Home() {
             );
           })}
         </div>
+      </div>
+
+      <div
+        className={`w-[250px] shadow-[0px_0px_10px_0px_gray] flex flex-col items-center fixed bg-white -translate-x-2/4 -translate-y-2/4 transition-transform duration-[0.2s] p-6 rounded-2xl scale-0 left-2/4 top-1/4 ${
+          title ? "-translate-x-2/4 -translate-y-2/4 scale-100" : ""
+        }`}
+      >
+        <div className="text-xl font-bold mb-4">{title}</div>
+        <button
+          className="bg-black text-white w-full h-9 text-lg font-semibold border-none rounded-md drop-shadow-[0px_4px_#ddd] hover:drop-shadow-none"
+          onClick={reset}
+        >
+          Nuevo Juego
+        </button>
       </div>
     </div>
   );
